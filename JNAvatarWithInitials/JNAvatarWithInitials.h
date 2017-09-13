b390947b88e8572a03ffdc83258f626d96919e1b
mohammadnabulsi <mohammad.s.nabulsi@gmail.com>
2017-09-13 23:42:07 +0300

    Initial Commit

---
+++ JNAvatarWithInitials/JNAvatarWithInitials.h
@@ -0,0 +1,19 @@
+//
+//  JNAvatarWithInitials.h
+//  JNAvatarWithInitials
+//
+//  Created by Mohammad Nabulsi on 9/13/17.
+//  Copyright © 2017 Mohammad Nabulsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JNAvatarWithInitials.
+FOUNDATION_EXPORT double JNAvatarWithInitialsVersionNumber;
+
+//! Project version string for JNAvatarWithInitials.
+FOUNDATION_EXPORT const unsigned char JNAvatarWithInitialsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JNAvatarWithInitials/PublicHeader.h>
+
+
